caf4e171acf0771989dd15a6a3be24a1acefd822
Atul Bhatt <atulbhatt61@gmail.com>
2025-08-19 13:41:36 +0530

    window, render loop, process events

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use glfw::{Action, Context, Key};
+use std::sync::mpsc::Receiver;
+
+const SCR_WIDTH: u32 = 800;
+const SCR_HEIGHT: u32 = 600;
+
 fn main() {
-    println!("Hello, world!");
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core));
+
+    #[cfg(target_os = "windows")]
+    glfw.window_hint(glfw::WindowHint::OpenGlForwardCompat(true));
+
+    // create window
+    let (mut window, events) = glfw.create_window(SCR_WIDTH, SCR_HEIGHT, "Gears", glfw::WindowMode::Windowed)
+            .expect("Failed to create window");
+
+    window.make_current();
+    window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+
+    // load function pointers
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    // render loop
+    while !window.should_close() {
+        process_events(&mut window, &events);
+        window.swap_buffers();
+        glfw.poll_events();
+    }
+}
+
+fn process_events(window: &mut glfw::Window, events: &Receiver<(f64, glfw::WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => {
+                unsafe { gl::Viewport(0, 0, width, height) }
+            }
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => window.set_should_close(true),
+            _ => {}
+        }
+    }
 }
